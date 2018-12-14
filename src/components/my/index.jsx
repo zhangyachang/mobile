41f49711f821a058f7372f82f1c30886a864f1e1
xiaojiaoya <937741304@qq.com>
2018-12-14 18:00:14 +0800

    验证es6

--- src/components/my/index.jsx
+++ src/components/my/index.jsx
@@ -2,9 +2,26 @@ import React, {Component} from 'react';
 
 import TopPageInfo  from "../base/toppageinfo";
 import FootNav from '../base/footnav'
-
+import './index.scss'
 class My extends Component {
+  constructor() {
+    super();
+
+    this.name = 'construe里面的自定义属性';
+
+  }
+  state = {
+    age: '11',
+    name: '111'
+  };
+
+  goudan = [];
+
+
   render() {
+    console.log('打印什么东西啊');
+    console.log(this);
+
     return (
       <div>
         <TopPageInfo
